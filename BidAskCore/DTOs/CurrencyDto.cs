70d9b1ece5d11ac64ba07352f78bdf3fe7f5df5b
Fil Mihaylov <filmihaylov1989@gmail.com>
2019-07-13 20:51:09 +0300

    initialization

---
+++ BidAskCore/DTOs/CurrencyDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidAskCore.DTOs
+{
+    class CurrencyDto
+    {
+        public decimal USD { get; set; }
+        public decimal EUR { get; set; }
+    }
+}
